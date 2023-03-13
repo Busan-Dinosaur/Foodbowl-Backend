0e65af5a6be2e5d429e002e16b03ce7685fd8bf3
woo-chang <clllickme@naver.com>
2023-03-13 10:33:42 +0900

    feat: 프로젝트 초기 구성

---
+++ src/main/java/org/dinosaur/foodbowl/FoodbowlApplication.java
@@ -0,0 +1,13 @@
+package org.dinosaur.foodbowl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodbowlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodbowlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dinosaur/foodbowl/FoodbowlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dinosaur.foodbowl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodbowlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
