7d67926992d3de40718fe39b363f4c5f69f71571
woo-chang <clllickme@naver.com>
2023-04-07 17:13:44 +0900

    feat: 역할 레포지토리 구현

---
+++ src/main/java/org/dinosaur/foodbowl/domain/member/dao/RoleRepository.java
@@ -0,0 +1,7 @@
+package org.dinosaur.foodbowl.domain.member.dao;
+
+import org.dinosaur.foodbowl.domain.member.entity.Role;
+import org.springframework.data.repository.Repository;
+
+public interface RoleRepository extends Repository<Role, Long> {
+}
