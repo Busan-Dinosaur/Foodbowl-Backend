e49c4309763dc73bd327b94a408e1bbea2808ca4
woo-chang <clllickme@naver.com>
2023-04-07 17:37:57 +0900

    feat: 멤버 역할 엔티티 구현

---
+++ src/main/java/org/dinosaur/foodbowl/domain/member/entity/MemberRole.java
@@ -0,0 +1,44 @@
+package org.dinosaur.foodbowl.domain.member.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.dinosaur.foodbowl.global.entity.BaseEntity;
+
+@Getter
+@Entity
+@Table(name = "member_role")
+@EqualsAndHashCode(of = {"id"})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberRole extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+    @Builder
+    private MemberRole(Member member, Role role) {
+        this.member = member;
+        this.role = role;
+    }
+}
