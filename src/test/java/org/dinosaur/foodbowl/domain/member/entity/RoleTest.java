5f55ff0726eb5b898b3bf55b356454c9fe9192e1
woo-chang <clllickme@naver.com>
2023-04-08 15:49:58 +0900

    test: EnumSource를 활용하도록 테스트 수정

--- src/test/java/org/dinosaur/foodbowl/domain/member/entity/RoleTest.java
+++ src/test/java/org/dinosaur/foodbowl/domain/member/entity/RoleTest.java
@@ -5,18 +5,20 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.dinosaur.foodbowl.domain.member.entity.Role.RoleType;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 class RoleTest {
 
     @DisplayName("역할 타입에 해당하는 역할을 생성한다.")
-    @Test
-    void createRoleByType() {
-        Role role = Role.from(RoleType.ROLE_회원);
+    @EnumSource
+    @ParameterizedTest
+    void createRoleByType(RoleType roleType) {
+        Role role = Role.from(roleType);
 
         assertAll(
-                () -> assertThat(role.getId()).isEqualTo(1L),
-                () -> assertThat(role.getRoleType()).isEqualTo(RoleType.ROLE_회원)
+                () -> assertThat(role.getId()).isEqualTo(roleType.getId()),
+                () -> assertThat(role.getRoleType()).isEqualTo(roleType)
         );
     }
 }
