6b27aa8d23a43f52d2434acc1b231f54a927da21
woo-chang <clllickme@naver.com>
2023-04-09 23:25:39 +0900

    feat: photo 레포지토리 구현

---
+++ src/main/java/org/dinosaur/foodbowl/domain/photo/repository/PhotoRepository.java
@@ -0,0 +1,7 @@
+package org.dinosaur.foodbowl.domain.photo.repository;
+
+import org.dinosaur.foodbowl.domain.photo.entity.Photo;
+import org.springframework.data.repository.Repository;
+
+public interface PhotoRepository extends Repository<Photo, Long> {
+}
