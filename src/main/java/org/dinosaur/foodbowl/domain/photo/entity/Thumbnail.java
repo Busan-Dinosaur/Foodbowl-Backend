73908c0799cac801db7a71df67b4e81ff5157c8f
woo-chang <clllickme@naver.com>
2023-04-09 23:31:55 +0900

    feat: thumbnail 엔티티 구현

--- src/main/java/org/dinosaur/foodbowl/domain/photo/entity/Thumbnail.java
+++ src/main/java/org/dinosaur/foodbowl/domain/photo/entity/Thumbnail.java
@@ -6,15 +6,38 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.dinosaur.foodbowl.global.entity.BaseEntity;
 
 @Getter
 @Entity
 @Table(name = "thumbnail")
-public class Thumbnail {
+@EqualsAndHashCode(of = {"id"}, callSuper = false)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Thumbnail extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false, updatable = false)
     private Long id;
+
+    @Column(name = "path", nullable = false, length = 512)
+    private String path;
+
+    @Column(name = "width", nullable = false)
+    private Integer width;
+
+    @Column(name = "height", nullable = false)
+    private Integer height;
+
+    @Builder
+    private Thumbnail(String path, Integer width, Integer height) {
+        this.path = path;
+        this.width = width;
+        this.height = height;
+    }
 }
