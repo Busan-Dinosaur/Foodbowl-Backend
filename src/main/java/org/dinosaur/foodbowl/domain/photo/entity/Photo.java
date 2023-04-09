00063bb6d7d947bfcf0f8bb75cef8dfe71fc894d
woo-chang <clllickme@naver.com>
2023-04-09 23:24:47 +0900

    feat: photo 엔티티 구현

---
+++ src/main/java/org/dinosaur/foodbowl/domain/photo/entity/Photo.java
@@ -0,0 +1,44 @@
+package org.dinosaur.foodbowl.domain.photo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.dinosaur.foodbowl.domain.post.entity.Post;
+import org.dinosaur.foodbowl.global.entity.BaseEntity;
+
+@Getter
+@Entity
+@Table(name = "photo")
+@EqualsAndHashCode(of = {"id"}, callSuper = false)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Photo extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @Column(name = "path", nullable = false)
+    private String path;
+
+    @Builder
+    private Photo(Post post, String path) {
+        this.post = post;
+        this.path = path;
+    }
+}
