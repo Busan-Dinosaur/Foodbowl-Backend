394258883e6aab9f7a40458050420f419e905113
woo-chang <clllickme@naver.com>
2023-04-09 23:32:52 +0900

    feat: thumbnail 레포지토리 구현

---
+++ src/main/java/org/dinosaur/foodbowl/domain/photo/repository/ThumbnailRepository.java
@@ -0,0 +1,7 @@
+package org.dinosaur.foodbowl.domain.photo.repository;
+
+import org.dinosaur.foodbowl.domain.photo.entity.Thumbnail;
+import org.springframework.data.repository.Repository;
+
+public interface ThumbnailRepository extends Repository<Thumbnail, Long> {
+}
