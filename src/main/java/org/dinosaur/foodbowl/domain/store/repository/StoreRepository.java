5009ee39529a2660219221df630b51846e1c804c
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-04-12 23:11:39 +0900

    feat: StoreRepository 생성

---
+++ src/main/java/org/dinosaur/foodbowl/domain/store/repository/StoreRepository.java
@@ -0,0 +1,8 @@
+package org.dinosaur.foodbowl.domain.store.repository;
+
+import org.dinosaur.foodbowl.domain.store.entity.Store;
+import org.springframework.data.repository.Repository;
+
+public interface StoreRepository extends Repository<Store, Long> {
+
+}
