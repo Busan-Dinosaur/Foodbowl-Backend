8af0aaa927cf7f1d2b931e97b2c89dde17e1930f
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-04-13 23:53:56 +0900

    feat(post): 게시글 엔티티 구현

--- src/main/java/org/dinosaur/foodbowl/domain/post/entity/Post.java
+++ src/main/java/org/dinosaur/foodbowl/domain/post/entity/Post.java
@@ -2,14 +2,24 @@ package org.dinosaur.foodbowl.domain.post.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.dinosaur.foodbowl.domain.member.entity.Member;
+import org.dinosaur.foodbowl.domain.photo.entity.Thumbnail;
+import org.dinosaur.foodbowl.domain.store.entity.Store;
 import org.dinosaur.foodbowl.global.entity.BaseEntity;
 
 @Getter
@@ -21,6 +31,39 @@ public class Post extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false, updatable = false)
+    @Column(name = "id", updatable = false)
     private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @NotNull
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @NotNull
+    @JoinColumn(name = "thumbnail_id")
+    private Thumbnail thumbnail;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @NotNull
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @NotNull
+    @Lob
+    @Column(name = "content")
+    private String content;
+
+    @NotNull
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Builder
+    private Post(Member member, Thumbnail thumbnail, Store store, String content) {
+        this.member = member;
+        this.thumbnail = thumbnail;
+        this.store = store;
+        this.content = content;
+        this.updatedAt = LocalDateTime.now();
+    }
 }
---
+++ src/main/java/org/dinosaur/foodbowl/domain/post/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package org.dinosaur.foodbowl.domain.post.repository;
+
+import org.dinosaur.foodbowl.domain.post.entity.Post;
+import org.springframework.data.repository.Repository;
+
+public interface PostRepository extends Repository<Post, Long> {
+}
