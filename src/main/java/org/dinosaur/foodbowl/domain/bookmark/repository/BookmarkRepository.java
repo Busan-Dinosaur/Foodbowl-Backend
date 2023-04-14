ca19f388a22ef3d4c68f3aa7da30e7022c905ec5
This2sho <zx00018@naver.com>
2023-04-14 13:56:28 +0900

    feat(bookmark): Bookmark 레포지토리 구현

---
+++ src/main/java/org/dinosaur/foodbowl/domain/bookmark/repository/BookmarkRepository.java
@@ -0,0 +1,7 @@
+package org.dinosaur.foodbowl.domain.bookmark.repository;
+
+import org.dinosaur.foodbowl.domain.bookmark.entity.Bookmark;
+import org.springframework.data.repository.Repository;
+
+public interface BookmarkRepository extends Repository<Bookmark, Long> {
+}
