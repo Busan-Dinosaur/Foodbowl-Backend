59747a228cb124c8cba5a2769ce4134b8e448faa
woo-chang <clllickme@naver.com>
2023-04-22 14:10:18 +0900

    test: 생성시간만 존재하는 엔티티 테스트 삭제

--- src/test/java/org/dinosaur/foodbowl/global/entity/AuditingEntityTest.java
+++ src/test/java/org/dinosaur/foodbowl/global/entity/AuditingEntityTest.java
@@ -3,8 +3,6 @@ package org.dinosaur.foodbowl.global.entity;
 import org.dinosaur.foodbowl.RepositoryTest;
 import org.dinosaur.foodbowl.domain.member.entity.Member;
 import org.dinosaur.foodbowl.domain.member.repository.MemberRepository;
-import org.dinosaur.foodbowl.domain.photo.entity.Thumbnail;
-import org.dinosaur.foodbowl.domain.photo.repository.ThumbnailRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,23 +13,6 @@ class AuditingEntityTest extends RepositoryTest {
 
     @Autowired
     private MemberRepository memberRepository;
-
-    @Autowired
-    private ThumbnailRepository thumbnailRepository;
-
-    @Test
-    @DisplayName("생성 시간만 존재하는 엔티티를 저장한다.")
-    void saveEntityWithCreatedTime() {
-        Thumbnail thumbnail = Thumbnail.builder()
-                .path("static/foodbowl")
-                .width(300)
-                .height(300)
-                .build();
-
-        Thumbnail savedThumbnail = thumbnailRepository.save(thumbnail);
-
-        assertThat(savedThumbnail.getCreatedAt()).isNotNull();
-    }
 
     @Test
     @DisplayName("생성 시간과 수정 시간이 존재하는 엔티티를 저장한다.")
