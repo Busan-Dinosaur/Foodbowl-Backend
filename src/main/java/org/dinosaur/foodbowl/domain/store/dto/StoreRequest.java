96be60e289d57dd95e51b4fdad7f465f96935b44
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-04-22 21:30:59 +0900

    refactor(store): @Data -> @Getter, @Setter로 변경

--- src/main/java/org/dinosaur/foodbowl/domain/store/dto/StoreRequest.java
+++ src/main/java/org/dinosaur/foodbowl/domain/store/dto/StoreRequest.java
@@ -6,10 +6,12 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class StoreRequest {
