0c220764086f62bdd16fcf31c87591bbfe020c88
woo-chang <clllickme@naver.com>
2023-04-28 16:18:30 +0900

    chore(AppleClient): url 경로 구분

--- src/main/java/org/dinosaur/foodbowl/domain/auth/apple/AppleClient.java
+++ src/main/java/org/dinosaur/foodbowl/domain/auth/apple/AppleClient.java
@@ -3,9 +3,9 @@ package org.dinosaur.foodbowl.domain.auth.apple;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "apple-public-key-client", url = "https://appleid.apple.com/auth/")
+@FeignClient(name = "apple-public-key-client", url = "https://appleid.apple.com")
 public interface AppleClient {
 
-    @GetMapping("keys")
+    @GetMapping("/auth/keys")
     ApplePublicKeys getApplePublicKeys();
 }
