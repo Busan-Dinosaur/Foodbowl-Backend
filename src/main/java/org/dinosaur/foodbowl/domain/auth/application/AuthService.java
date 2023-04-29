c6b8a8efd7dc8f2f91c51d95ba64478f8c660c93
woo-chang <clllickme@naver.com>
2023-04-29 19:50:25 +0900

    feat: 애플 로그인 시 리프레쉬 토큰 발급 기능 구현

--- src/main/java/org/dinosaur/foodbowl/domain/auth/application/AuthService.java
+++ src/main/java/org/dinosaur/foodbowl/domain/auth/application/AuthService.java
@@ -2,13 +2,16 @@ package org.dinosaur.foodbowl.domain.auth.application;
 
 import lombok.RequiredArgsConstructor;
 import org.dinosaur.foodbowl.domain.auth.apple.AppleOAuthUserProvider;
+import org.dinosaur.foodbowl.domain.auth.dto.FoodbowlTokenDto;
 import org.dinosaur.foodbowl.domain.auth.dto.request.AppleLoginRequestDto;
 import org.dinosaur.foodbowl.domain.auth.dto.response.ApplePlatformUserResponseDto;
-import org.dinosaur.foodbowl.domain.auth.dto.response.AppleTokenResponseDto;
 import org.dinosaur.foodbowl.domain.member.repository.MemberRepository;
 import org.dinosaur.foodbowl.global.config.security.jwt.JwtTokenProvider;
 import org.dinosaur.foodbowl.global.exception.FoodbowlException;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.dinosaur.foodbowl.domain.member.entity.Member.SocialType;
 import static org.dinosaur.foodbowl.domain.member.entity.Role.RoleType;
@@ -21,8 +24,9 @@ public class AuthService {
     private final MemberRepository memberRepository;
     private final JwtTokenProvider jwtTokenProvider;
     private final AppleOAuthUserProvider appleOAuthUserProvider;
+    private final RedisTemplate<String, Object> redisTemplate;
 
-    public AppleTokenResponseDto appleLogin(AppleLoginRequestDto appleLoginRequestDto) {
+    public FoodbowlTokenDto appleLogin(AppleLoginRequestDto appleLoginRequestDto) {
         ApplePlatformUserResponseDto applePlatformUserResponseDto =
                 appleOAuthUserProvider.extractApplePlatformUser(appleLoginRequestDto.getAppleToken());
         String socialId = applePlatformUserResponseDto.getSocialId();
@@ -30,8 +34,20 @@ public class AuthService {
         return memberRepository.findBySocialTypeAndSocialId(SocialType.APPLE, socialId)
                 .map(member -> {
                     String accessToken = jwtTokenProvider.createAccessToken(member.getId(), RoleType.ROLE_회원);
-                    return new AppleTokenResponseDto(accessToken);
+                    String refreshToken = jwtTokenProvider.createRefreshToken(member.getId());
+                    registerRefreshToken(member.getId(), refreshToken);
+                    return new FoodbowlTokenDto(accessToken, refreshToken);
                 })
                 .orElseThrow(() -> new FoodbowlException(APPLE_NOT_REGISTER));
+    }
+
+    private void registerRefreshToken(Long memberId, String refreshToken) {
+        redisTemplate.opsForValue()
+                .set(
+                        String.valueOf(memberId),
+                        refreshToken,
+                        jwtTokenProvider.getValidRefreshMilliSecond(),
+                        TimeUnit.MILLISECONDS
+                );
     }
 }
