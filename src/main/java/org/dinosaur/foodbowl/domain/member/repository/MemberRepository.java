26ec6e61075a88005d0318abc35c49b127afb2b1
woo-chang <clllickme@naver.com>
2023-04-29 17:45:49 +0900

    feat(MemberRepository): 소셜 타입과 소셜 ID로 멤버 조회하는 기능 구현

--- src/main/java/org/dinosaur/foodbowl/domain/member/repository/MemberRepository.java
+++ src/main/java/org/dinosaur/foodbowl/domain/member/repository/MemberRepository.java
@@ -3,7 +3,13 @@ package org.dinosaur.foodbowl.domain.member.repository;
 import org.dinosaur.foodbowl.domain.member.entity.Member;
 import org.springframework.data.repository.Repository;
 
+import java.util.Optional;
+
+import static org.dinosaur.foodbowl.domain.member.entity.Member.SocialType;
+
 public interface MemberRepository extends Repository<Member, Long> {
 
     Member save(Member member);
+
+    Optional<Member> findBySocialTypeAndSocialId(SocialType socialType, String socialId);
 }
