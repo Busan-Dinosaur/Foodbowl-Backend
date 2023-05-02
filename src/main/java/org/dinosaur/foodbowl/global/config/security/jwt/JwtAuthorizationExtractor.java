3a58475b5a189162e5a8957dcc1efd863fd546c6
woo-chang <clllickme@naver.com>
2023-05-03 01:01:22 +0900

    refactor: auth delimiter 상수화

--- src/main/java/org/dinosaur/foodbowl/global/config/security/jwt/JwtAuthorizationExtractor.java
+++ src/main/java/org/dinosaur/foodbowl/global/config/security/jwt/JwtAuthorizationExtractor.java
@@ -11,6 +11,7 @@ public class JwtAuthorizationExtractor {
 
     private static final String AUTHENTICATION_TYPE = "Bearer";
     private static final String AUTHENTICATION_HEADER_KEY = "Authorization";
+    private static final String AUTHENTICATION_DELIMITER = " ";
     private static final int TOKEN_INDEX = 1;
 
     public Optional<String> extractAccessToken(HttpServletRequest request) {
@@ -19,7 +20,7 @@ public class JwtAuthorizationExtractor {
         while (headers.hasMoreElements()) {
             String value = headers.nextElement();
             if (value.toLowerCase().startsWith(AUTHENTICATION_TYPE.toLowerCase())) {
-                return Optional.ofNullable(value.split(" ")[TOKEN_INDEX]);
+                return Optional.ofNullable(value.split(AUTHENTICATION_DELIMITER)[TOKEN_INDEX]);
             }
         }
         return Optional.empty();
