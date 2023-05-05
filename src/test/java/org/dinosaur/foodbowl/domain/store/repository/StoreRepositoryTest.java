2d8b7e2752ce0a1079f3bdfd0a3cc4c386ac60d5
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-05-05 11:02:41 +0900

    test(store): StoreRepository 테스트 작성

---
+++ src/test/java/org/dinosaur/foodbowl/domain/store/repository/StoreRepositoryTest.java
@@ -0,0 +1,89 @@
+package org.dinosaur.foodbowl.domain.store.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+import org.dinosaur.foodbowl.RepositoryTest;
+import org.dinosaur.foodbowl.domain.store.entity.Address;
+import org.dinosaur.foodbowl.domain.store.entity.Store;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class StoreRepositoryTest extends RepositoryTest {
+
+    @Autowired
+    StoreRepository storeRepository;
+
+    @Test
+    @DisplayName("가게 ID에 해당하는 정보를 가져온다.")
+    void findByIdSuccess() {
+        Store savedStore = storeRepository.save(createStore());
+
+        Store findStore = storeRepository.findById(savedStore.getId()).get();
+
+        assertThat(findStore).isEqualTo(savedStore);
+    }
+
+    @Test
+    @DisplayName("가게 ID에 해당하는 가게가 없으면 빈 값을 반환한다.")
+    void findOneWithEmptySuccess() {
+        storeRepository.save(createStore());
+
+        Optional<Store> store = storeRepository.findById(Long.MAX_VALUE);
+
+        assertThat(store.isEmpty()).isTrue();
+    }
+
+    @Test
+    @DisplayName("가게 이름에 해당하는 가게 정보를 가져온다.")
+    void findByStoreNameSuccess() {
+        Store savedStore = storeRepository.save(createStore());
+
+        Optional<Store> findStore = storeRepository.findByStoreName(savedStore.getStoreName());
+
+        assertThat(findStore.get()).isEqualTo(savedStore);
+    }
+
+    @Test
+    @DisplayName("가게 이름에 해당하는 가게가 없으면 빈 값을 반환한다.")
+    void findByStoreNameWithEmptySuccess() {
+        storeRepository.save(createStore());
+
+        Optional<Store> findStore = storeRepository.findByStoreName("푸드볼");
+
+        assertThat(findStore.isEmpty()).isTrue();
+    }
+
+    @Test
+    @DisplayName("모든 가게를 반환한다.")
+    void findAllSuccess() {
+        assertThat(storeRepository.findAll()).hasSizeGreaterThanOrEqualTo(0);
+    }
+
+    private Store createStore() {
+        Address address = createAddress();
+        return Store.builder()
+                .address(address)
+                .storeName("작살치킨")
+                .build();
+    }
+
+    private Address createAddress() {
+        return Address.builder()
+                .addressName("서울시 송파구 방이동 방이로 1234")
+                .region1depthName("서울시")
+                .region2depthName("송파구")
+                .region3depthName("방이동")
+                .roadName("방이로")
+                .mainBuildingNo("1234")
+                .subBuildingNo("14층 1400호")
+                .undergroundYN("N")
+                .buildingName("작살치킨 빌딩")
+                .zoneNo("12345")
+                .x(BigDecimal.valueOf(127.3437575))
+                .y(BigDecimal.valueOf(37.12567))
+                .build();
+    }
+}
