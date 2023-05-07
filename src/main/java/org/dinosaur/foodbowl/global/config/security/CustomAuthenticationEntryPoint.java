49f1b2f648849dbee0b5c437795f88b972f12e34
woo-chang <clllickme@naver.com>
2023-05-07 17:32:04 +0900

    feat: 애플 로그아웃 API 구현

--- src/main/java/org/dinosaur/foodbowl/global/config/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/org/dinosaur/foodbowl/global/config/security/CustomAuthenticationEntryPoint.java
@@ -14,6 +14,7 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
             AuthenticationException authException) throws IOException, ServletException {
+        response.getWriter().write("인증에 실패하였습니다.");
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
     }
 }
