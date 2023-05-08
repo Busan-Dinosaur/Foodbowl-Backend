8ffaf35ca9b98ed24ca454e6e511013787d9b767
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-05-08 22:50:46 +0900

    chore(StoreRequest): validation 메시지 수정

--- src/main/java/org/dinosaur/foodbowl/domain/store/dto/StoreRequest.java
+++ src/main/java/org/dinosaur/foodbowl/domain/store/dto/StoreRequest.java
@@ -18,23 +18,23 @@ import lombok.NoArgsConstructor;
 public class StoreRequest {
 
     @NotBlank(message = "가게 이름은 반드시 입력되어야 합니다.")
-    @Size(max = 100, message = "길이는 {max}까지 가능합니다.")
+    @Size(max = 100, message = "가게 이름의 길이는 {max}까지 가능합니다.")
     private String storeName;
 
     @NotBlank(message = "주소는 반드시 입력되어야 합니다.")
-    @Size(max = 512, message = "길이는 {max}까지 가능합니다.")
+    @Size(max = 512, message = "주소의 길이는 {max}까지 가능합니다.")
     private String addressName;
 
     @NotBlank(message = "광역시 또는 도는 반드시 입력되어야 합니다.")
-    @Size(max = 45, message = "길이는 {max}까지 가능합니다.")
+    @Size(max = 45, message = "광역시 또는 도의 길이는 {max}까지 가능합니다.")
     private String region1depthName;
 
     @NotBlank(message = "시/군/구는 반드시 입력되어야 합니다.")
-    @Size(max = 45, message = "길이는 {max}까지 가능합니다.")
+    @Size(max = 45, message = "시/군/구의 길이는 {max}까지 가능합니다.")
     private String region2depthName;
 
     @NotBlank(message = "읍/면/동은 반드시 입력되어야 합니다.")
-    @Size(max = 45, message = "길이는 {max}까지 가능합니다.")
+    @Size(max = 45, message = "읍/면/동의 길이는 {max}까지 가능합니다.")
     private String region3depthName;
 
     private String roadName;
