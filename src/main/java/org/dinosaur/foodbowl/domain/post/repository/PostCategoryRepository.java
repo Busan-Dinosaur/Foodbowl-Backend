bbff0adfaa93e31deb5ac8ee24155bcc7f337556
woo-chang <clllickme@naver.com>
2023-05-11 00:20:56 +0900

    feat: 게시글의 카테고리 목록 삭제 기능 구현

--- src/main/java/org/dinosaur/foodbowl/domain/post/repository/PostCategoryRepository.java
+++ src/main/java/org/dinosaur/foodbowl/domain/post/repository/PostCategoryRepository.java
@@ -1,8 +1,15 @@
 package org.dinosaur.foodbowl.domain.post.repository;
 
+import java.util.List;
+import org.dinosaur.foodbowl.domain.post.entity.Post;
 import org.dinosaur.foodbowl.domain.post.entity.PostCategory;
 import org.springframework.data.repository.Repository;
 
 public interface PostCategoryRepository extends Repository<PostCategory, Long> {
 
+    List<PostCategory> findAll();
+
+    PostCategory save(PostCategory postCategory);
+
+    void deleteAllByPost(Post post);
 }
