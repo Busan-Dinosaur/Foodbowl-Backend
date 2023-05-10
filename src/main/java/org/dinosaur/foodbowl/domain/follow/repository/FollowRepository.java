a4b7f1773e34e17736661e7fb54e93d7d84bc6aa
woo-chang <clllickme@naver.com>
2023-05-11 00:10:52 +0900

    feat: 멤버의 팔로우, 팔로잉 목록 삭제 기능 구현

--- src/main/java/org/dinosaur/foodbowl/domain/follow/repository/FollowRepository.java
+++ src/main/java/org/dinosaur/foodbowl/domain/follow/repository/FollowRepository.java
@@ -1,8 +1,17 @@
 package org.dinosaur.foodbowl.domain.follow.repository;
 
+import java.util.List;
 import org.dinosaur.foodbowl.domain.follow.entity.Follow;
+import org.dinosaur.foodbowl.domain.member.entity.Member;
 import org.springframework.data.repository.Repository;
 
 public interface FollowRepository extends Repository<Follow, Long> {
 
+    List<Follow> findAll();
+
+    Follow save(Follow follow);
+
+    void deleteAllByFollowing(Member member);
+
+    void deleteAllByFollower(Member member);
 }
