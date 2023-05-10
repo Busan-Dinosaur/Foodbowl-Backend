29d9cbfbe1ebdcc215213f1deb0c54d288310c39
woo-chang <clllickme@naver.com>
2023-05-11 00:07:42 +0900

    test: 게시글에 작성된 댓글 목록 삭제 기능 구현

--- src/test/java/org/dinosaur/foodbowl/domain/comment/repository/CommentRepositoryTest.java
+++ src/test/java/org/dinosaur/foodbowl/domain/comment/repository/CommentRepositoryTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.dinosaur.foodbowl.RepositoryTest;
 import org.dinosaur.foodbowl.domain.member.entity.Member;
+import org.dinosaur.foodbowl.domain.post.entity.Post;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,17 @@ class CommentRepositoryTest extends RepositoryTest {
         commentTestSupport.builder().member(member).build();
 
         commentRepository.deleteAllByMember(member);
+
+        assertThat(commentRepository.findAll()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("게시글에 작성된 댓글 목록을 삭제한다.")
+    void deleteAllByPost() {
+        Post post = postTestSupport.postBuilder().build();
+        commentTestSupport.builder().post(post).build();
+
+        commentRepository.deleteAllByPost(post);
 
         assertThat(commentRepository.findAll()).isEmpty();
     }
