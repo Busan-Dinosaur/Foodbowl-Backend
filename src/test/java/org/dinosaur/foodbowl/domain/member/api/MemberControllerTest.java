6ff4adef6f09610f0f5a74b4ec193607a1ebf75e
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-05-14 21:20:04 +0900

    test(MemberController): 요청 파라미터가 없는 경우 테스트 추가

--- src/test/java/org/dinosaur/foodbowl/domain/member/api/MemberControllerTest.java
+++ src/test/java/org/dinosaur/foodbowl/domain/member/api/MemberControllerTest.java
@@ -38,6 +38,18 @@ class MemberControllerTest extends MockApiTest {
         private final String token = jwtTokenProvider.createAccessToken(1L, RoleType.ROLE_회원);
 
         @Test
+        @DisplayName("요청 파라미터가 없으면 BAD REQUEST가 발생한다.")
+        void checkDuplicateFailWithNoParams() throws Exception {
+            given(memberService.checkDuplicate(any())).willReturn(new DuplicateCheckResponse(false));
+
+            mockMvc.perform(get("/api/v1/members/check-nickname")
+                            .header("Authorization", "Bearer " + token)
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isBadRequest())
+                    .andDo(print());
+        }
+
+        @Test
         @DisplayName("요청 닉네임이 존재하면 true를 반환한다.")
         void checkDuplicateTrue() throws Exception {
             given(memberService.checkDuplicate(any())).willReturn(new DuplicateCheckResponse(true));
