a39ad30d85c7fac351a99b4802f99baf1cbdee54
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-05-17 21:04:03 +0900

    test(CommentRepository): 댓글 저장 테스트 추가

--- src/main/java/org/dinosaur/foodbowl/domain/post/repository/PostRepository.java
+++ src/main/java/org/dinosaur/foodbowl/domain/post/repository/PostRepository.java
@@ -1,10 +1,13 @@
 package org.dinosaur.foodbowl.domain.post.repository;
 
 import java.util.List;
+import java.util.Optional;
 import org.dinosaur.foodbowl.domain.post.entity.Post;
 import org.springframework.data.repository.Repository;
 
 public interface PostRepository extends Repository<Post, Long> {
+
+    Optional<Post> findById(Long id);
 
     List<Post> findAll();
 
