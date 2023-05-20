9bdd11d1f4006c014cb792e1c0226a1006c549c3
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-05-20 18:13:28 +0900

    refactor(Comment): 댓글 등록 테스트 검증 필드 추가

--- src/test/java/org/dinosaur/foodbowl/domain/comment/repository/CommentRepositoryTest.java
+++ src/test/java/org/dinosaur/foodbowl/domain/comment/repository/CommentRepositoryTest.java
@@ -1,12 +1,12 @@
 package org.dinosaur.foodbowl.domain.comment.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.dinosaur.foodbowl.RepositoryTest;
 import org.dinosaur.foodbowl.domain.comment.entity.Comment;
 import org.dinosaur.foodbowl.domain.member.entity.Member;
 import org.dinosaur.foodbowl.domain.post.entity.Post;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,7 +30,8 @@ class CommentRepositoryTest extends RepositoryTest {
 
         Comment savedComment = commentRepository.save(comment);
 
-        Assertions.assertAll(
+        assertAll(
+                () -> assertThat(savedComment.getId()).isNotNull(),
                 () -> assertThat(savedComment.getMember()).isEqualTo(comment.getMember()),
                 () -> assertThat(savedComment.getPost()).isEqualTo(comment.getPost()),
                 () -> assertThat(savedComment.getParent()).isNull(),
