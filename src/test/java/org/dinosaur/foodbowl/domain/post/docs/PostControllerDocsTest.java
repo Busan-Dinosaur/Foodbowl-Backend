9e1323609f929e14895d10ff86df00a219296feb
woo-chang <clllickme@naver.com>
2023-05-25 23:55:42 +0900

    docs: 게시글 가게 위치 목록 조회 문서화

--- src/test/java/org/dinosaur/foodbowl/domain/post/docs/PostControllerDocsTest.java
+++ src/test/java/org/dinosaur/foodbowl/domain/post/docs/PostControllerDocsTest.java
@@ -16,12 +16,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.dinosaur.foodbowl.MockApiTest;
 import org.dinosaur.foodbowl.domain.member.entity.Role.RoleType;
 import org.dinosaur.foodbowl.domain.post.api.PostController;
 import org.dinosaur.foodbowl.domain.post.application.PostService;
+import org.dinosaur.foodbowl.domain.post.dto.response.PostStoreMarkerResponse;
 import org.dinosaur.foodbowl.domain.post.dto.response.PostThumbnailResponse;
 import org.dinosaur.foodbowl.global.config.security.jwt.JwtTokenProvider;
 import org.dinosaur.foodbowl.global.dto.PageResponse;
@@ -107,6 +109,55 @@ public class PostControllerDocsTest extends MockApiTest {
                         ),
                         queryParameters(
                                 parameterDescriptors
+                        ),
+                        responseFields(
+                                responseFieldDescriptors
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("게시글 가게 위치 정보 목록 조회를 문서화한다.")
+    void responseListOfPostStoreLocationInformation() throws Exception {
+        List<PostStoreMarkerResponse> response = List.of(
+                new PostStoreMarkerResponse(
+                        1L,
+                        "깐부치킨",
+                        "서울특별시 송파구 잠실본동 올림픽로8길 19",
+                        BigDecimal.valueOf(127.65),
+                        BigDecimal.valueOf(55.65)
+                ),
+                new PostStoreMarkerResponse(
+                        2L,
+                        "서오릉피자",
+                        "서울특별시 송파구 가락로 71 빌라드그리움 1층 101,102호",
+                        BigDecimal.valueOf(58.77),
+                        BigDecimal.valueOf(33.77)
+                )
+        );
+        given(postService.findPostStoreMarkers(anyLong())).willReturn(response);
+
+        var headerDescriptors = new HeaderDescriptor[]{
+                headerWithName(HttpHeaders.AUTHORIZATION).description("서버에서 발급한 엑세스 토큰")
+        };
+
+        var responseFieldDescriptors = new FieldDescriptor[]{
+                fieldWithPath("[]").description("게시글 가게 위치 정보 목록"),
+                fieldWithPath("[].storeId").description("가게 ID"),
+                fieldWithPath("[].storeName").description("가게 이름"),
+                fieldWithPath("[].storeAddress").description("가게 주소"),
+                fieldWithPath("[].x").description("경도"),
+                fieldWithPath("[].y").description("위도")
+        };
+
+        mockMvc.perform(get("/api/v1/posts/markers")
+                        .header("Authorization", "Bearer " + jwtTokenProvider.createAccessToken(1L, RoleType.ROLE_회원))
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("api-v1-posts-markers",
+                        requestHeaders(
+                                headerDescriptors
                         ),
                         responseFields(
                                 responseFieldDescriptors
