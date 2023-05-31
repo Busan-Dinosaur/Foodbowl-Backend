ace999c45f7d979854f3a109c73dbf866f68c7af
This2sho <zx00018@naver.com>
2023-05-31 21:24:07 +0900

    docs(Post): 게시글 생성 기능 문서화

--- src/test/java/org/dinosaur/foodbowl/domain/post/docs/PostControllerDocsTest.java
+++ src/test/java/org/dinosaur/foodbowl/domain/post/docs/PostControllerDocsTest.java
@@ -2,29 +2,40 @@ package org.dinosaur.foodbowl.domain.post.docs;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.multipart;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestPartFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.partWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParts;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.dinosaur.foodbowl.MockApiTest;
 import org.dinosaur.foodbowl.domain.member.entity.Role.RoleType;
 import org.dinosaur.foodbowl.domain.post.api.PostController;
 import org.dinosaur.foodbowl.domain.post.application.PostService;
+import org.dinosaur.foodbowl.domain.post.dto.request.PostCreateRequest;
 import org.dinosaur.foodbowl.domain.post.dto.response.PostThumbnailResponse;
 import org.dinosaur.foodbowl.global.config.security.jwt.JwtTokenProvider;
 import org.dinosaur.foodbowl.global.dto.PageResponse;
+import org.dinosaur.foodbowl.testsupport.fixture.MultipartFileFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +43,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.restdocs.headers.HeaderDescriptor;
 import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.restdocs.request.ParameterDescriptor;
@@ -39,11 +52,55 @@ import org.springframework.restdocs.request.ParameterDescriptor;
 @WebMvcTest(PostController.class)
 public class PostControllerDocsTest extends MockApiTest {
 
-    @Autowired
-    private JwtTokenProvider jwtTokenProvider;
-
     @MockBean
     private PostService postService;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    @Test
+    @DisplayName("게시글 생성을 문서화한다.")
+    void createPost() throws Exception {
+        PostCreateRequest postCreateRequest = new PostCreateRequest(1L, List.of("카페", "한식"), "맛있다");
+
+        given(postService.save(anyLong(), any(PostCreateRequest.class), anyList())).willReturn(1L);
+
+        MockMultipartFile request = new MockMultipartFile("request", "", "application/json",
+                objectMapper.writeValueAsString(postCreateRequest).getBytes(StandardCharsets.UTF_8));
+
+        var headerDescriptors = new HeaderDescriptor[]{
+                headerWithName(HttpHeaders.AUTHORIZATION).description("서버에서 발급한 액세스 토큰")
+        };
+
+        mockMvc.perform(multipart("/api/v1/posts")
+                        .file(request)
+                        .file(MultipartFileFixture.get())
+                        .header("Authorization", "Bearer " + jwtTokenProvider.createAccessToken(1L, RoleType.ROLE_회원))
+                        .contentType(MediaType.MULTIPART_FORM_DATA)
+                        .characterEncoding(StandardCharsets.UTF_8))
+                .andExpect(status().isCreated())
+                .andExpect(redirectedUrl("/api/v1/posts/1"))
+                .andDo(document("api-v1-posts",
+                        requestHeaders(
+                                headerDescriptors
+                        ),
+                        requestParts(
+                                partWithName("request").description("게시글 정보"),
+                                partWithName("imageFiles").description("게시글 사진들")
+                        ),
+                        requestPartFields("request",
+                                fieldWithPath("storeId").description("가게 ID"),
+                                fieldWithPath("categoryNames").description("카테고리"),
+                                fieldWithPath("content").description("후기")
+                        ),
+                        responseHeaders(
+                                headerWithName(HttpHeaders.LOCATION).description("생성된 게시글의 URI")
+                        )
+                ));
+    }
 
     @Test
     @DisplayName("프로필 게시글 목록 조회를 문서화한다.")
