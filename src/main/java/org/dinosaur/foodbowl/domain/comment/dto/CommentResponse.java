330203f94926c31d9c2fdde655a90bebb558915f
gray <68818952+Kim0914@users.noreply.github.com>
2023-07-03 21:40:41 +0900

    Feature/#37 댓글 목록 조회 기능을 구현한다. (#75)
    * feat: 페이징 조건에 맞도록 댓글 조회하는 기능 추가
    
    * feat: 게시글에 존재하는 모든 댓글을 조회하는 기능 추가
    
    * feat: 게시글에 존재하는 모든 댓글을 조회하는 요청을 처리하는 기능 추가
    
    * refactor: MemberTestSupport 롤백
    
    * docs: 댓글 조회 rest docs 작성
    
    * docs: 댓글 조회 page parameter 수정
    
    * refactor: CommentResponse 정적 팩토리 메서드 명 변경
    
    * docs: 댓글 조회 rest docs 명 변경
    
    * docs: 댓글 조회 rest docs 명 변경

--- src/main/java/org/dinosaur/foodbowl/domain/comment/dto/CommentResponse.java
+++ src/main/java/org/dinosaur/foodbowl/domain/comment/dto/CommentResponse.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.dinosaur.foodbowl.domain.comment.entity.Comment;
+import org.dinosaur.foodbowl.domain.post.entity.Post;
 
 @Getter
 @AllArgsConstructor
@@ -14,14 +15,20 @@ public class CommentResponse {
 
     private Long commentId;
     private Long postId;
+    private Long memberId;
+    private String memberNickname;
+    private String memberThumbnailPath;
     private String message;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
-    public static CommentResponse from(Comment comment) {
+    public static CommentResponse of(Comment comment, Post post) {
         return new CommentResponse(
                 comment.getId(),
-                comment.getPost().getId(),
+                post.getId(),
+                comment.getMember().getId(),
+                comment.getMember().getNickname(),
+                comment.getMember().getThumbnail().getPath(),
                 comment.getMessage(),
                 comment.getCreatedAt(),
                 comment.getUpdatedAt()
