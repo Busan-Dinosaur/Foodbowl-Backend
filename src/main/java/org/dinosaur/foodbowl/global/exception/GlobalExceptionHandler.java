5b716c86e1a18b56ea629a3570fd88fae926605a
woo-chang <clllickme@naver.com>
2023-07-24 22:40:29 +0900

    refactor: (#74) ExceptionHandler 확장

--- src/main/java/org/dinosaur/foodbowl/global/exception/GlobalExceptionHandler.java
+++ src/main/java/org/dinosaur/foodbowl/global/exception/GlobalExceptionHandler.java
@@ -5,10 +5,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<ExceptionResponse> handleException(Exception e) {
