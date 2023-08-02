02d6a500272129bcfb06b7cc6c71dee13df6d873
woo-chang <clllickme@naver.com>
2023-08-02 15:17:27 +0900

    refactor: (#87) Security 기능 리팩토링

--- src/main/java/org/dinosaur/foodbowl/global/presentation/security/CustomAccessDeniedHandler.java
+++ src/main/java/org/dinosaur/foodbowl/global/presentation/security/CustomAccessDeniedHandler.java
@@ -1,14 +1,23 @@
 package org.dinosaur.foodbowl.global.presentation.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.dinosaur.foodbowl.domain.auth.exception.AuthExceptionType;
+import org.dinosaur.foodbowl.global.exception.ExceptionResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public void handle(
@@ -16,6 +25,9 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
             HttpServletResponse response,
             AccessDeniedException accessDeniedException
     ) throws IOException {
-        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+        objectMapper.writeValue(response.getWriter(), ExceptionResponse.from(AuthExceptionType.FORBIDDEN));
     }
 }
--- src/main/java/org/dinosaur/foodbowl/global/presentation/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/org/dinosaur/foodbowl/global/presentation/security/CustomAuthenticationEntryPoint.java
@@ -1,14 +1,23 @@
 package org.dinosaur.foodbowl.global.presentation.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.dinosaur.foodbowl.domain.auth.exception.AuthExceptionType;
+import org.dinosaur.foodbowl.global.exception.ExceptionResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public void commence(
@@ -16,7 +25,9 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
             HttpServletResponse response,
             AuthenticationException authException
     ) throws IOException {
-        response.getWriter().write("인증에 실패하였습니다.");
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+        objectMapper.writeValue(response.getWriter(), ExceptionResponse.from(AuthExceptionType.NOT_AUTHENTICATION));
     }
 }
