5741677bcb032fb1501883621cd5546acf2afdf4
woo-chang <clllickme@naver.com>
2023-08-03 22:39:36 +0900

    chore: (#87) 스웨거 응답 문서화

--- src/main/java/org/dinosaur/foodbowl/global/exception/ExceptionResponse.java
+++ src/main/java/org/dinosaur/foodbowl/global/exception/ExceptionResponse.java
@@ -1,6 +1,14 @@
 package org.dinosaur.foodbowl.global.exception;
 
-public record ExceptionResponse(String errorCode, String message) {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "예외 응답")
+public record ExceptionResponse(
+        @Schema(description = "에러 코드", example = "SERVER-100")
+        String errorCode,
+        @Schema(description = "에러 메시지", example = "알 수 없는 서버 에러가 발생했습니다.")
+        String message
+) {
 
     public static ExceptionResponse from(ExceptionType exceptionType) {
         return new ExceptionResponse(exceptionType.getErrorCode(), exceptionType.getMessage());
