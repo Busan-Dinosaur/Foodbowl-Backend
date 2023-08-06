ea3ec47381012d38f51979f39ff84b4ade85b1ae
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-08-06 14:53:35 +0900

    feat: (#86) 학교를 저장하고 이름을 기준으로 조회하는 기능 추가

--- src/main/java/org/dinosaur/foodbowl/domain/store/persistence/SchoolRepository.java
+++ src/main/java/org/dinosaur/foodbowl/domain/store/persistence/SchoolRepository.java
@@ -1,7 +1,12 @@
 package org.dinosaur.foodbowl.domain.store.persistence;
 
+import java.util.Optional;
 import org.dinosaur.foodbowl.domain.store.domain.School;
 import org.springframework.data.repository.Repository;
 
 public interface SchoolRepository extends Repository<School, Long> {
+
+    Optional<School> findByName(String name);
+
+    School save(School school);
 }
---
+++ src/test/java/org/dinosaur/foodbowl/domain/store/persistence/SchoolRepositoryTest.java
@@ -0,0 +1,47 @@
+package org.dinosaur.foodbowl.domain.store.persistence;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.math.BigDecimal;
+import org.dinosaur.foodbowl.PersistenceTest;
+import org.dinosaur.foodbowl.domain.store.domain.School;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@SuppressWarnings("NonAsciiCharacters")
+class SchoolRepositoryTest extends PersistenceTest {
+
+    @Autowired
+    private SchoolRepository schoolRepository;
+
+    @Test
+    void 학교를_저장한다() {
+        School school = generateSchool();
+
+        School saveSchool = schoolRepository.save(school);
+
+        assertAll(
+                () -> assertThat(saveSchool.getId()).isNotNull(),
+                () -> assertThat(saveSchool.getName()).isEqualTo(school.getName())
+        );
+    }
+
+    @Test
+    void 이름으로_학교를_조회한다() {
+        School school = generateSchool();
+        schoolRepository.save(school);
+
+        School findSchool = schoolRepository.findByName(school.getName()).get();
+
+        assertThat(findSchool.getName()).isEqualTo(school.getName());
+    }
+
+    private School generateSchool() {
+        return School.builder()
+                .name("부산대학교")
+                .x(BigDecimal.valueOf(123.12451))
+                .y(BigDecimal.valueOf(37.124125))
+                .build();
+    }
+}
