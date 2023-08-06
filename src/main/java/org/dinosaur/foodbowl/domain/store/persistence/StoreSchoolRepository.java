90194d572d7f71a8126214e2067508831004663c
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-08-06 22:08:49 +0900

    feat: (#86) 가게와 학교의 정보를 저장하고 조회하는 기능 추가

--- src/main/java/org/dinosaur/foodbowl/domain/store/persistence/StoreSchoolRepository.java
+++ src/main/java/org/dinosaur/foodbowl/domain/store/persistence/StoreSchoolRepository.java
@@ -1,7 +1,12 @@
 package org.dinosaur.foodbowl.domain.store.persistence;
 
+import java.util.List;
 import org.dinosaur.foodbowl.domain.store.domain.StoreSchool;
 import org.springframework.data.repository.Repository;
 
 public interface StoreSchoolRepository extends Repository<StoreSchool, Long> {
+
+    StoreSchool save(StoreSchool storeSchool);
+
+    List<StoreSchool> findAllBySchoolName(String name);
 }
---
+++ src/test/java/org/dinosaur/foodbowl/domain/store/persistence/StoreSchoolRepositoryTest.java
@@ -0,0 +1,55 @@
+package org.dinosaur.foodbowl.domain.store.persistence;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.util.List;
+import org.dinosaur.foodbowl.PersistenceTest;
+import org.dinosaur.foodbowl.domain.store.domain.School;
+import org.dinosaur.foodbowl.domain.store.domain.Store;
+import org.dinosaur.foodbowl.domain.store.domain.StoreSchool;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@SuppressWarnings("NonAsciiCharacters")
+class StoreSchoolRepositoryTest extends PersistenceTest {
+
+    @Autowired
+    private StoreSchoolRepository storeSchoolRepository;
+
+    @Test
+    void 가게와_학교의_관계를_저장한다() {
+        Store store = storeTestPersister.builder().build();
+        School school = schoolTestPersister.builder().build();
+        StoreSchool storeSchool = StoreSchool.builder()
+                .store(store)
+                .school(school)
+                .build();
+
+        StoreSchool saveStoreSchool = storeSchoolRepository.save(storeSchool);
+
+        assertAll(
+                () -> assertThat(saveStoreSchool.getId()).isNotNull(),
+                () -> assertThat(saveStoreSchool.getStore()).isEqualTo(store),
+                () -> assertThat(saveStoreSchool.getSchool()).isEqualTo(school)
+        );
+    }
+
+    @Test
+    void 학교와_관련된_가게를_가져온다() {
+        Store store = storeTestPersister.builder().build();
+        School school = schoolTestPersister.builder().build();
+        StoreSchool storeSchool = StoreSchool.builder()
+                .store(store)
+                .school(school)
+                .build();
+        storeSchoolRepository.save(storeSchool);
+
+        List<StoreSchool> storeSchools = storeSchoolRepository.findAllBySchoolName(school.getName());
+        List<Store> stores = storeSchools.stream()
+                .map(StoreSchool::getStore)
+                .toList();
+
+        assertThat(stores).contains(store);
+    }
+}
