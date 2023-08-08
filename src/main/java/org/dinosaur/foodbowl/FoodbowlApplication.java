cc408cd12a7ae7692cf166e0a9cd8006d2ff668b
woo-chang <clllickme@naver.com>
2023-08-08 15:18:43 +0900

    refactor: (#87) 사용하지 않는 어노테이션 제거

--- src/main/java/org/dinosaur/foodbowl/FoodbowlApplication.java
+++ src/main/java/org/dinosaur/foodbowl/FoodbowlApplication.java
@@ -2,9 +2,7 @@ package org.dinosaur.foodbowl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@ConfigurationPropertiesScan
 @SpringBootApplication
 public class FoodbowlApplication {
 
