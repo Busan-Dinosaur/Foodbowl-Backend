c68bc33ed4fa7bc24a4af6b0a43b66affb4fd10a
woo-chang <clllickme@naver.com>
2023-08-09 08:43:47 +0900

    refactor: (#102) 팔로우 도메인 패키지 수정

--- src/main/java/org/dinosaur/foodbowl/domain/follow/persistence/FollowRepository.java
+++ src/main/java/org/dinosaur/foodbowl/domain/follow/persistence/FollowRepository.java
@@ -1,6 +1,6 @@
-package org.dinosaur.foodbowl.domain.member.persistence;
+package org.dinosaur.foodbowl.domain.follow.persistence;
 
-import org.dinosaur.foodbowl.domain.member.domain.Follow;
+import org.dinosaur.foodbowl.domain.follow.domain.Follow;
 import org.springframework.data.repository.Repository;
 
 public interface FollowRepository extends Repository<Follow, Long> {
