c3c39da48893077004f1daefafd0fd313277d21a
woo-chang <clllickme@naver.com>
2023-08-21 20:47:27 +0900

    feat: (#120) 팔로워 목록 페이징 조회 기능 구현

--- src/main/java/org/dinosaur/foodbowl/domain/follow/persistence/FollowRepository.java
+++ src/main/java/org/dinosaur/foodbowl/domain/follow/persistence/FollowRepository.java
@@ -3,11 +3,17 @@ package org.dinosaur.foodbowl.domain.follow.persistence;
 import java.util.Optional;
 import org.dinosaur.foodbowl.domain.follow.domain.Follow;
 import org.dinosaur.foodbowl.domain.member.domain.Member;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.Repository;
 
 public interface FollowRepository extends Repository<Follow, Long> {
 
     Optional<Follow> findById(Long id);
+
+    @EntityGraph(attributePaths = {"follower", "follower.memberThumbnail", "follower.memberThumbnail.thumbnail"})
+    Slice<Follow> findAllByFollowing(Member following, Pageable pageable);
 
     Optional<Follow> findByFollowingAndFollower(Member following, Member follower);
 
