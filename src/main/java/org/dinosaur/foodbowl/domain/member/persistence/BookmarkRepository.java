e41a26da1feb96aa336ddce49efcc4d257478378
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-08-25 15:39:32 +0900

    feat: (#124) 북마크 저장, 조회 기능 추가

--- src/main/java/org/dinosaur/foodbowl/domain/member/persistence/BookmarkRepository.java
+++ src/main/java/org/dinosaur/foodbowl/domain/member/persistence/BookmarkRepository.java
@@ -1,7 +1,14 @@
 package org.dinosaur.foodbowl.domain.member.persistence;
 
+import java.util.Optional;
 import org.dinosaur.foodbowl.domain.member.domain.Bookmark;
+import org.dinosaur.foodbowl.domain.member.domain.Member;
+import org.dinosaur.foodbowl.domain.store.domain.Store;
 import org.springframework.data.repository.Repository;
 
 public interface BookmarkRepository extends Repository<Bookmark, Long> {
+
+    Bookmark save(Bookmark bookmark);
+
+    Optional<Bookmark> findByMemberAndStore(Member member, Store store);
 }
