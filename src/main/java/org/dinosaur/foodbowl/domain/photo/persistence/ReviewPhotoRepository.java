c85167cc9f2dd3dee0bc05a6dd7b3065ae5f8909
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-08-29 17:04:26 +0900

    feat: (#118) 리뷰와 관련된 리뷰-사진 데이터를 가져오는 메서드 추가

--- src/main/java/org/dinosaur/foodbowl/domain/photo/persistence/ReviewPhotoRepository.java
+++ src/main/java/org/dinosaur/foodbowl/domain/photo/persistence/ReviewPhotoRepository.java
@@ -1,9 +1,15 @@
 package org.dinosaur.foodbowl.domain.photo.persistence;
 
+import java.util.List;
 import org.dinosaur.foodbowl.domain.photo.domain.ReviewPhoto;
+import org.dinosaur.foodbowl.domain.review.domain.Review;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.Repository;
 
 public interface ReviewPhotoRepository extends Repository<ReviewPhoto, Long>, ReviewPhotoCustomRepository {
 
     ReviewPhoto save(ReviewPhoto reviewPhoto);
+
+    @EntityGraph(attributePaths = {"photo"})
+    List<ReviewPhoto> findAllByReview(Review review);
 }
--- src/test/java/org/dinosaur/foodbowl/domain/photo/persistence/ReviewPhotoRepositoryTest.java
+++ src/test/java/org/dinosaur/foodbowl/domain/photo/persistence/ReviewPhotoRepositoryTest.java
@@ -3,6 +3,7 @@ package org.dinosaur.foodbowl.domain.photo.persistence;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
+import java.util.List;
 import org.dinosaur.foodbowl.domain.photo.domain.Photo;
 import org.dinosaur.foodbowl.domain.photo.domain.ReviewPhoto;
 import org.dinosaur.foodbowl.domain.review.domain.Review;
@@ -15,6 +16,27 @@ class ReviewPhotoRepositoryTest extends PersistenceTest {
 
     @Autowired
     private ReviewPhotoRepository reviewPhotoRepository;
+
+    @Test
+    void 리뷰에_해당하는_리뷰_사진_데이터를_조회한다() {
+        Review review = reviewTestPersister.builder().save();
+        Photo photo1 = photoTestPersister.builder().save();
+        Photo photo2 = photoTestPersister.builder().save();
+        ReviewPhoto reviewPhoto1 = ReviewPhoto.builder()
+                .review(review)
+                .photo(photo1)
+                .build();
+        ReviewPhoto reviewPhoto2 = ReviewPhoto.builder()
+                .review(review)
+                .photo(photo2)
+                .build();
+        reviewPhotoRepository.save(reviewPhoto1);
+        reviewPhotoRepository.save(reviewPhoto2);
+
+        List<ReviewPhoto> reviewPhotos = reviewPhotoRepository.findAllByReview(review);
+
+        assertThat(reviewPhotos).contains(reviewPhoto1, reviewPhoto2);
+    }
 
     @Test
     void 리뷰_사진_엔티티를_저장한다() {
