fe598a06bacbbc9c3f3d63cf6634d9a053ba5f05
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-08-29 15:24:44 +0900

    feat: (#118) 리뷰 작성자를 확인하는 메서드 추가

--- src/test/java/org/dinosaur/foodbowl/domain/review/domain/ReviewTest.java
+++ src/test/java/org/dinosaur/foodbowl/domain/review/domain/ReviewTest.java
@@ -8,6 +8,7 @@ import org.dinosaur.foodbowl.domain.store.domain.Store;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -34,5 +35,61 @@ class ReviewTest {
                 .build();
 
         assertThat(review.getContent()).isEqualTo("맛있어요");
+    }
+
+    @Test
+    void 리뷰_작성자가_아니면_true_반환한다() {
+        Member member = Member.builder()
+                .socialType(SocialType.APPLE)
+                .socialId("1")
+                .email("email@email.com")
+                .nickname("hello")
+                .introduction("hello world")
+                .build();
+        ReflectionTestUtils.setField(member, "id", 1L);
+        Member otherMember = Member.builder()
+                .socialType(SocialType.APPLE)
+                .socialId("2")
+                .email("email2@email.com")
+                .nickname("helloAA")
+                .introduction("hello world2")
+                .build();
+        ReflectionTestUtils.setField(otherMember, "id", 2L);
+        Store store = Store.builder()
+                .storeName("농민백암순대")
+                .storeUrl("http://foodbowl.com")
+                .phone("02-123-4567")
+                .build();
+        Review review = Review.builder()
+                .member(member)
+                .store(store)
+                .content("맛있어요")
+                .build();
+
+        assertThat(review.isNotOwnerOf(otherMember)).isTrue();
+    }
+
+    @Test
+    void 리뷰_작성자와_동일하면_false_반환한다() {
+        Member member = Member.builder()
+                .socialType(SocialType.APPLE)
+                .socialId("1")
+                .email("email@email.com")
+                .nickname("hello")
+                .introduction("hello world")
+                .build();
+        ReflectionTestUtils.setField(member, "id", 1L);
+        Store store = Store.builder()
+                .storeName("농민백암순대")
+                .storeUrl("http://foodbowl.com")
+                .phone("02-123-4567")
+                .build();
+        Review review = Review.builder()
+                .member(member)
+                .store(store)
+                .content("맛있어요")
+                .build();
+
+        assertThat(review.isNotOwnerOf(member)).isFalse();
     }
 }
