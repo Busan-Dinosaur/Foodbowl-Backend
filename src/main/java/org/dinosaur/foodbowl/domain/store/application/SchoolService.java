5f4a7c77972028128a72700e3ae6677b1465d1f2
woo-chang <clllickme@naver.com>
2023-09-01 22:25:57 +0900

    feat: (#133) 모든 학교 목록 이름순 조회 기능 구현

--- src/main/java/org/dinosaur/foodbowl/domain/store/application/SchoolService.java
+++ src/main/java/org/dinosaur/foodbowl/domain/store/application/SchoolService.java
@@ -3,9 +3,12 @@ package org.dinosaur.foodbowl.domain.store.application;
 import static org.dinosaur.foodbowl.domain.store.exception.SchoolExceptionType.DUPLICATE_ERROR;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.dinosaur.foodbowl.domain.store.domain.School;
+import org.dinosaur.foodbowl.domain.store.domain.vo.SchoolName;
+import org.dinosaur.foodbowl.domain.store.dto.response.SchoolsResponse;
 import org.dinosaur.foodbowl.domain.store.persistence.SchoolRepository;
 import org.dinosaur.foodbowl.global.exception.BadRequestException;
 import org.springframework.stereotype.Service;
@@ -17,9 +20,20 @@ public class SchoolService {
 
     private final SchoolRepository schoolRepository;
 
+    @Transactional(readOnly = true)
+    public Optional<School> findByName(String name) {
+        return schoolRepository.findByName(new SchoolName(name));
+    }
+
+    @Transactional(readOnly = true)
+    public SchoolsResponse getSchools() {
+        List<School> schools = schoolRepository.findAllByOrderByName();
+        return SchoolsResponse.from(schools);
+    }
+
     @Transactional
     public School save(String name, BigDecimal x, BigDecimal y) {
-        schoolRepository.findByName_Name(name).ifPresent(
+        schoolRepository.findByName(new SchoolName(name)).ifPresent(
                 existingSchool -> {
                     throw new BadRequestException(DUPLICATE_ERROR);
                 });
@@ -30,10 +44,5 @@ public class SchoolService {
                 .y(y)
                 .build();
         return schoolRepository.save(school);
-    }
-
-    @Transactional(readOnly = true)
-    public Optional<School> findByName(String name) {
-        return schoolRepository.findByName_Name(name);
     }
 }
--- src/test/java/org/dinosaur/foodbowl/domain/store/application/SchoolServiceTest.java
+++ src/test/java/org/dinosaur/foodbowl/domain/store/application/SchoolServiceTest.java
@@ -4,7 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.math.BigDecimal;
+import java.util.List;
 import org.dinosaur.foodbowl.domain.store.domain.School;
+import org.dinosaur.foodbowl.domain.store.dto.response.SchoolsResponse;
 import org.dinosaur.foodbowl.global.exception.BadRequestException;
 import org.dinosaur.foodbowl.test.IntegrationTest;
 import org.junit.jupiter.api.Nested;
@@ -16,6 +18,35 @@ class SchoolServiceTest extends IntegrationTest {
 
     @Autowired
     private SchoolService schoolService;
+
+    @Nested
+    class 학교_조회_시 {
+
+        @Test
+        void 존재하는_학교로_조회한다() {
+            String name = "부산대학교";
+            BigDecimal x = BigDecimal.valueOf(123.1234);
+            BigDecimal y = BigDecimal.valueOf(37.12421);
+            schoolService.save(name, x, y);
+
+            assertThat(schoolService.findByName(name)).isPresent();
+        }
+
+        @Test
+        void 존재하지_않는_학교로_조회한다() {
+            assertThat(schoolService.findByName("우테코대학교")).isEmpty();
+        }
+    }
+
+    @Test
+    void 모든_학교_목록을_이름순으로_조회한다() {
+        School schoolA = schoolTestPersister.builder().name("부산대학교").save();
+        School schoolB = schoolTestPersister.builder().name("강남대학교").save();
+
+        SchoolsResponse response = schoolService.getSchools();
+
+        assertThat(response).usingRecursiveComparison().isEqualTo(SchoolsResponse.from(List.of(schoolB, schoolA)));
+    }
 
     @Nested
     class 학교를_저장_시 {
@@ -41,25 +72,6 @@ class SchoolServiceTest extends IntegrationTest {
             assertThatThrownBy(() -> schoolService.save(name, x, y))
                     .isInstanceOf(BadRequestException.class)
                     .hasMessage("이미 존재하는 학교입니다.");
-        }
-    }
-
-    @Nested
-    class 학교_조회_시 {
-
-        @Test
-        void 존재하는_학교로_조회한다() {
-            String name = "부산대학교";
-            BigDecimal x = BigDecimal.valueOf(123.1234);
-            BigDecimal y = BigDecimal.valueOf(37.12421);
-            schoolService.save(name, x, y);
-
-            assertThat(schoolService.findByName(name)).isPresent();
-        }
-
-        @Test
-        void 존재하지_않는_학교로_조회한다() {
-            assertThat(schoolService.findByName("우테코대학교")).isEmpty();
         }
     }
 }
