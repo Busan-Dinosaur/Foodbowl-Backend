8c674dc86d2f25ea2ff1b075e35969763ece6302
woo-chang <clllickme@naver.com>
2023-09-01 14:01:04 +0900

    refactor: (#141) 사용하지 않는 dto 제거

--- src/main/java/org/dinosaur/foodbowl/domain/auth/application/jwt/JwtTokenValid.java
+++
@@ -1,6 +0,0 @@
-package org.dinosaur.foodbowl.domain.auth.application.jwt;
-
-import org.dinosaur.foodbowl.global.exception.ExceptionType;
-
-public record JwtTokenValid(boolean isValid, ExceptionType exceptionType) {
-}
