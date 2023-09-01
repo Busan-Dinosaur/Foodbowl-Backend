1602eaa5809f98f46c88e393906fc399792fc2b0
woo-chang <clllickme@naver.com>
2023-09-01 22:32:55 +0900

    feat: (#133) 모든 학교 목록 이름순 조회 API 구현

--- src/main/java/org/dinosaur/foodbowl/domain/store/presentation/StoreController.java
+++ src/main/java/org/dinosaur/foodbowl/domain/store/presentation/StoreController.java
@@ -1,8 +1,10 @@
 package org.dinosaur.foodbowl.domain.store.presentation;
 
 import lombok.RequiredArgsConstructor;
+import org.dinosaur.foodbowl.domain.store.application.SchoolService;
 import org.dinosaur.foodbowl.domain.store.application.StoreService;
 import org.dinosaur.foodbowl.domain.store.dto.response.CategoriesResponse;
+import org.dinosaur.foodbowl.domain.store.dto.response.SchoolsResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class StoreController implements StoreControllerDocs {
 
     private final StoreService storeService;
+    private final SchoolService schoolService;
 
     @GetMapping("/categories")
     public ResponseEntity<CategoriesResponse> getCategories() {
         CategoriesResponse response = storeService.getCategories();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/schools")
+    public ResponseEntity<SchoolsResponse> getSchools() {
+        SchoolsResponse response = schoolService.getSchools();
         return ResponseEntity.ok(response);
     }
 }
--- src/main/java/org/dinosaur/foodbowl/domain/store/presentation/StoreControllerDocs.java
+++ src/main/java/org/dinosaur/foodbowl/domain/store/presentation/StoreControllerDocs.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.dinosaur.foodbowl.domain.store.dto.response.CategoriesResponse;
+import org.dinosaur.foodbowl.domain.store.dto.response.SchoolsResponse;
 import org.springframework.http.ResponseEntity;
 
 @Tag(name = "가게", description = "가게 API")
@@ -15,4 +16,11 @@ public interface StoreControllerDocs {
             description = "카테고리 목록 조회 성공"
     )
     ResponseEntity<CategoriesResponse> getCategories();
+
+    @Operation(summary = "학교 목록 조회", description = "DB에 존재하는 학교 목록을 조회한다.")
+    @ApiResponse(
+            responseCode = "200",
+            description = "학교 목록 조회 성공"
+    )
+    ResponseEntity<SchoolsResponse> getSchools();
 }
