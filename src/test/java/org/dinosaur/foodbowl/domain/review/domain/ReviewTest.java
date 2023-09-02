c8405597c89d41bab48ddee582650e82dcdec646
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-09-02 17:03:50 +0900

    chore: (#118) 동일한 메서드 테스트 Nested로 변경

--- src/test/java/org/dinosaur/foodbowl/domain/review/domain/ReviewTest.java
+++ src/test/java/org/dinosaur/foodbowl/domain/review/domain/ReviewTest.java
@@ -7,6 +7,7 @@ import org.dinosaur.foodbowl.domain.member.domain.vo.SocialType;
 import org.dinosaur.foodbowl.domain.store.domain.Store;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.util.ReflectionTestUtils;
 
@@ -37,59 +38,63 @@ class ReviewTest {
         assertThat(review.getContent()).isEqualTo("맛있어요");
     }
 
-    @Test
-    void 리뷰_작성자가_아니면_true_반환한다() {
-        Member member = Member.builder()
-                .socialType(SocialType.APPLE)
-                .socialId("1")
-                .email("email@email.com")
-                .nickname("hello")
-                .introduction("hello world")
-                .build();
-        ReflectionTestUtils.setField(member, "id", 1L);
-        Member otherMember = Member.builder()
-                .socialType(SocialType.APPLE)
-                .socialId("2")
-                .email("email2@email.com")
-                .nickname("helloAA")
-                .introduction("hello world2")
-                .build();
-        ReflectionTestUtils.setField(otherMember, "id", 2L);
-        Store store = Store.builder()
-                .storeName("농민백암순대")
-                .storeUrl("http://foodbowl.com")
-                .phone("02-123-4567")
-                .build();
-        Review review = Review.builder()
-                .member(member)
-                .store(store)
-                .content("맛있어요")
-                .build();
+    @Nested
+    class 리뷰_작성자_확인_메서드는 {
 
-        assertThat(review.isNotOwnerOf(otherMember)).isTrue();
-    }
+        @Test
+        void 작성자가_아니면_true_반환한다() {
+            Member member = Member.builder()
+                    .socialType(SocialType.APPLE)
+                    .socialId("1")
+                    .email("email@email.com")
+                    .nickname("hello")
+                    .introduction("hello world")
+                    .build();
+            ReflectionTestUtils.setField(member, "id", 1L);
+            Member otherMember = Member.builder()
+                    .socialType(SocialType.APPLE)
+                    .socialId("2")
+                    .email("email2@email.com")
+                    .nickname("helloAA")
+                    .introduction("hello world2")
+                    .build();
+            ReflectionTestUtils.setField(otherMember, "id", 2L);
+            Store store = Store.builder()
+                    .storeName("농민백암순대")
+                    .storeUrl("http://foodbowl.com")
+                    .phone("02-123-4567")
+                    .build();
+            Review review = Review.builder()
+                    .member(member)
+                    .store(store)
+                    .content("맛있어요")
+                    .build();
 
-    @Test
-    void 리뷰_작성자와_동일하면_false_반환한다() {
-        Member member = Member.builder()
-                .socialType(SocialType.APPLE)
-                .socialId("1")
-                .email("email@email.com")
-                .nickname("hello")
-                .introduction("hello world")
-                .build();
-        ReflectionTestUtils.setField(member, "id", 1L);
-        Store store = Store.builder()
-                .storeName("농민백암순대")
-                .storeUrl("http://foodbowl.com")
-                .phone("02-123-4567")
-                .build();
-        Review review = Review.builder()
-                .member(member)
-                .store(store)
-                .content("맛있어요")
-                .build();
+            assertThat(review.isNotOwnerOf(otherMember)).isTrue();
+        }
 
-        assertThat(review.isNotOwnerOf(member)).isFalse();
+        @Test
+        void 작성자와_동일하면_false_반환한다() {
+            Member member = Member.builder()
+                    .socialType(SocialType.APPLE)
+                    .socialId("1")
+                    .email("email@email.com")
+                    .nickname("hello")
+                    .introduction("hello world")
+                    .build();
+            ReflectionTestUtils.setField(member, "id", 1L);
+            Store store = Store.builder()
+                    .storeName("농민백암순대")
+                    .storeUrl("http://foodbowl.com")
+                    .phone("02-123-4567")
+                    .build();
+            Review review = Review.builder()
+                    .member(member)
+                    .store(store)
+                    .content("맛있어요")
+                    .build();
+
+            assertThat(review.isNotOwnerOf(member)).isFalse();
+        }
     }
 }
