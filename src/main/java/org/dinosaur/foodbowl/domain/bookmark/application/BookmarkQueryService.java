44ab14739ebf24a9e2dcc5bf51ff9c3a2ab51d38
woo-chang <clllickme@naver.com>
2023-09-30 22:35:02 +0900

    feat: (#103) 멤버의 북마크한 가게 목록 조회 기능 구현

---
+++ src/main/java/org/dinosaur/foodbowl/domain/bookmark/application/BookmarkQueryService.java
@@ -0,0 +1,27 @@
+package org.dinosaur.foodbowl.domain.bookmark.application;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.dinosaur.foodbowl.domain.bookmark.domain.Bookmark;
+import org.dinosaur.foodbowl.domain.bookmark.persistence.BookmarkRepository;
+import org.dinosaur.foodbowl.domain.member.domain.Member;
+import org.dinosaur.foodbowl.domain.store.domain.Store;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class BookmarkQueryService {
+
+    private final BookmarkRepository bookmarkRepository;
+
+    public Set<Store> getBookmarkStoresByMember(Member member) {
+        List<Bookmark> bookmarks = bookmarkRepository.findByMember(member);
+        return bookmarks.stream()
+                .map(Bookmark::getStore)
+                .collect(Collectors.toSet());
+    }
+}
