7bc68768ca999a53f70c25238857aa6823faa0e3
woo-chang <clllickme@naver.com>
2023-10-06 22:13:32 +0900

    refactor: (#131) 메서드 위치 컨벤션 정렬

--- src/test/java/org/dinosaur/foodbowl/domain/store/presentation/StoreControllerTest.java
+++ src/test/java/org/dinosaur/foodbowl/domain/store/presentation/StoreControllerTest.java
@@ -59,132 +59,57 @@ class StoreControllerTest extends PresentationTest {
     @MockBean
     private SchoolService schoolService;
 
-    @Nested
-    class 팔로잉_하는_유저의_리뷰가_존재하는_가게_목록_조회_시 {
+    @Test
+    void 가게_카테고리_목록_조회시_카테고리_목록과_200_응답을_반환한다() throws Exception {
+        CategoriesResponse categoriesResponse = new CategoriesResponse(
+                List.of(
+                        new CategoryResponse(1L, "카페"),
+                        new CategoryResponse(2L, "술집"))
+        );
+        given(storeService.getCategories()).willReturn(categoriesResponse);
 
-        private final String accessToken = jwtTokenProvider.createAccessToken(1L, ROLE_회원);
+        MvcResult mvcResult = mockMvc.perform(get("/v1/stores/categories")
+                        .header(AUTHORIZATION, BEARER + jwtTokenProvider.createAccessToken(1L, ROLE_회원)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+        String jsonResponse = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
+        CategoriesResponse result = objectMapper.readValue(jsonResponse, CategoriesResponse.class);
 
-        @Test
-        void 정상적인_요청이라면_200_응답을_반환한다() throws Exception {
-            mockingAuthMemberInResolver();
-            StoreMapBoundResponses response = new StoreMapBoundResponses(
-                    List.of(
-                            new StoreMapBoundResponse(
-                                    1L,
-                                    "가게 이름",
-                                    "가게 카테고리명",
-                                    "가게 주소",
-                                    "가게 URL",
-                                    123.3636,
-                                    32.3636,
-                                    5,
-                                    false
-                            )
-                    )
-            );
-            given(storeService.getStoresByFollowingInMapBounds(any(MapCoordinateRequest.class), any(Member.class)))
-                    .willReturn(response);
+        assertThat(result).usingRecursiveComparison().isEqualTo(categoriesResponse);
+    }
 
-            MvcResult mvcResult = mockMvc.perform(get("/v1/stores/followings")
-                            .header(AUTHORIZATION, BEARER + accessToken)
-                            .param("x", "123.3636")
-                            .param("y", "32.3636")
-                            .param("deltaX", "3.1212")
-                            .param("deltaY", "3.1212"))
-                    .andDo(print())
-                    .andExpect(status().isOk())
-                    .andReturn();
+    @Test
+    void 학교_목록_조회시_학교_목록과_200_응답을_반환한다() throws Exception {
+        SchoolsResponse schoolsResponse = new SchoolsResponse(
+                List.of(
+                        new SchoolResponse(
+                                1L,
+                                "강남대학교",
+                                "경기도 용인시 기흥구 강남로 40",
+                                BigDecimal.valueOf(127.125),
+                                BigDecimal.valueOf(35.12)
+                        ),
+                        new SchoolResponse(
+                                2L,
+                                "부산대학교",
+                                "부산광역시 금정구 부산대학로63번길 2",
+                                BigDecimal.valueOf(127.350),
+                                BigDecimal.valueOf(35.78)
+                        )
+                )
+        );
+        given(schoolService.getSchools()).willReturn(schoolsResponse);
 
-            String jsonResponse = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
-            StoreMapBoundResponses result = objectMapper.readValue(jsonResponse, StoreMapBoundResponses.class);
-            assertThat(result).usingRecursiveComparison().isEqualTo(response);
-        }
+        MvcResult mvcResult = mockMvc.perform(get("/v1/stores/schools")
+                        .header(AUTHORIZATION, BEARER + jwtTokenProvider.createAccessToken(1L, ROLE_회원)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+        String jsonResponse = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
+        SchoolsResponse result = objectMapper.readValue(jsonResponse, SchoolsResponse.class);
 
-        @Test
-        void 경도가_존재하지_않으면_400_응답을_반환한다() throws Exception {
-            mockingAuthMemberInResolver();
-
-            mockMvc.perform(get("/v1/stores/followings")
-                            .header(AUTHORIZATION, BEARER + accessToken)
-                            .param("y", "32.3636")
-                            .param("deltaX", "3.12")
-                            .param("deltaY", "3.12"))
-                    .andDo(print())
-                    .andExpect(status().isBadRequest());
-        }
-
-        @Test
-        void 위도가_존재하지_않으면_400_응답을_반환한다() throws Exception {
-            mockingAuthMemberInResolver();
-
-            mockMvc.perform(get("/v1/stores/followings")
-                            .header(AUTHORIZATION, BEARER + accessToken)
-                            .param("x", "123.3636")
-                            .param("deltaX", "3.12")
-                            .param("deltaY", "3.12"))
-                    .andDo(print())
-                    .andExpect(status().isBadRequest());
-        }
-
-        @Test
-        void 경도_증가값이_존재하지_않으면_400_응답을_반환한다() throws Exception {
-            mockingAuthMemberInResolver();
-
-            mockMvc.perform(get("/v1/stores/followings")
-                            .header(AUTHORIZATION, BEARER + accessToken)
-                            .param("x", "123.3636")
-                            .param("y", "32.3636")
-                            .param("deltaY", "3.12"))
-                    .andDo(print())
-                    .andExpect(status().isBadRequest());
-        }
-
-        @Test
-        void 위도_증가값이_존재하지_않으면_400_응답을_반환한다() throws Exception {
-            mockingAuthMemberInResolver();
-
-            mockMvc.perform(get("/v1/stores/followings")
-                            .header(AUTHORIZATION, BEARER + accessToken)
-                            .param("x", "123.3636")
-                            .param("y", "32.3636")
-                            .param("deltaX", "3.12"))
-                    .andDo(print())
-                    .andExpect(status().isBadRequest());
-        }
-
-        @ParameterizedTest
-        @ValueSource(strings = {"-1", "0"})
-        void 경도_증가값이_0이상의_양수가_아니라면_400_응답을_반환한다(String deltaX) throws Exception {
-            mockingAuthMemberInResolver();
-
-            mockMvc.perform(get("/v1/stores/followings")
-                            .header(AUTHORIZATION, BEARER + accessToken)
-                            .param("x", "123.3636")
-                            .param("y", "32.3636")
-                            .param("deltaX", deltaX)
-                            .param("deltaY", "3.12"))
-                    .andDo(print())
-                    .andExpect(status().isBadRequest())
-                    .andExpect(jsonPath("errorCode").value("CLIENT-101"))
-                    .andExpect(jsonPath("$.message").value(containsString("경도 증가값은 0이상의 양수만 가능합니다.")));
-        }
-
-        @ParameterizedTest
-        @ValueSource(strings = {"-1", "0"})
-        void 위도_증가값이_0이상의_양수가_아니라면_400_응답을_반환한다(String deltaY) throws Exception {
-            mockingAuthMemberInResolver();
-
-            mockMvc.perform(get("/v1/stores/followings")
-                            .header(AUTHORIZATION, BEARER + accessToken)
-                            .param("x", "123.3636")
-                            .param("y", "32.3636")
-                            .param("deltaX", "3.12")
-                            .param("deltaY", deltaY))
-                    .andDo(print())
-                    .andExpect(status().isBadRequest())
-                    .andExpect(jsonPath("errorCode").value("CLIENT-101"))
-                    .andExpect(jsonPath("$.message").value(containsString("위도 증가값은 0이상의 양수만 가능합니다.")));
-        }
+        assertThat(result).usingRecursiveComparison().isEqualTo(schoolsResponse);
     }
 
     @Nested
@@ -322,56 +247,131 @@ class StoreControllerTest extends PresentationTest {
         }
     }
 
-    @Test
-    void 가게_카테고리_목록_조회시_카테고리_목록과_200_응답을_반환한다() throws Exception {
-        CategoriesResponse categoriesResponse = new CategoriesResponse(
-                List.of(
-                        new CategoryResponse(1L, "카페"),
-                        new CategoryResponse(2L, "술집"))
-        );
-        given(storeService.getCategories()).willReturn(categoriesResponse);
+    @Nested
+    class 팔로잉_하는_유저의_리뷰가_존재하는_가게_목록_조회_시 {
 
-        MvcResult mvcResult = mockMvc.perform(get("/v1/stores/categories")
-                        .header(AUTHORIZATION, BEARER + jwtTokenProvider.createAccessToken(1L, ROLE_회원)))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andReturn();
-        String jsonResponse = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
-        CategoriesResponse result = objectMapper.readValue(jsonResponse, CategoriesResponse.class);
+        private final String accessToken = jwtTokenProvider.createAccessToken(1L, ROLE_회원);
 
-        assertThat(result).usingRecursiveComparison().isEqualTo(categoriesResponse);
-    }
+        @Test
+        void 정상적인_요청이라면_200_응답을_반환한다() throws Exception {
+            mockingAuthMemberInResolver();
+            StoreMapBoundResponses response = new StoreMapBoundResponses(
+                    List.of(
+                            new StoreMapBoundResponse(
+                                    1L,
+                                    "가게 이름",
+                                    "가게 카테고리명",
+                                    "가게 주소",
+                                    "가게 URL",
+                                    123.3636,
+                                    32.3636,
+                                    5,
+                                    false
+                            )
+                    )
+            );
+            given(storeService.getStoresByFollowingInMapBounds(any(MapCoordinateRequest.class), any(Member.class)))
+                    .willReturn(response);
 
-    @Test
-    void 학교_목록_조회시_학교_목록과_200_응답을_반환한다() throws Exception {
-        SchoolsResponse schoolsResponse = new SchoolsResponse(
-                List.of(
-                        new SchoolResponse(
-                                1L,
-                                "강남대학교",
-                                "경기도 용인시 기흥구 강남로 40",
-                                BigDecimal.valueOf(127.125),
-                                BigDecimal.valueOf(35.12)
-                        ),
-                        new SchoolResponse(
-                                2L,
-                                "부산대학교",
-                                "부산광역시 금정구 부산대학로63번길 2",
-                                BigDecimal.valueOf(127.350),
-                                BigDecimal.valueOf(35.78)
-                        )
-                )
-        );
-        given(schoolService.getSchools()).willReturn(schoolsResponse);
+            MvcResult mvcResult = mockMvc.perform(get("/v1/stores/followings")
+                            .header(AUTHORIZATION, BEARER + accessToken)
+                            .param("x", "123.3636")
+                            .param("y", "32.3636")
+                            .param("deltaX", "3.1212")
+                            .param("deltaY", "3.1212"))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andReturn();
 
-        MvcResult mvcResult = mockMvc.perform(get("/v1/stores/schools")
-                        .header(AUTHORIZATION, BEARER + jwtTokenProvider.createAccessToken(1L, ROLE_회원)))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andReturn();
-        String jsonResponse = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
-        SchoolsResponse result = objectMapper.readValue(jsonResponse, SchoolsResponse.class);
+            String jsonResponse = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
+            StoreMapBoundResponses result = objectMapper.readValue(jsonResponse, StoreMapBoundResponses.class);
+            assertThat(result).usingRecursiveComparison().isEqualTo(response);
+        }
 
-        assertThat(result).usingRecursiveComparison().isEqualTo(schoolsResponse);
+        @Test
+        void 경도가_존재하지_않으면_400_응답을_반환한다() throws Exception {
+            mockingAuthMemberInResolver();
+
+            mockMvc.perform(get("/v1/stores/followings")
+                            .header(AUTHORIZATION, BEARER + accessToken)
+                            .param("y", "32.3636")
+                            .param("deltaX", "3.12")
+                            .param("deltaY", "3.12"))
+                    .andDo(print())
+                    .andExpect(status().isBadRequest());
+        }
+
+        @Test
+        void 위도가_존재하지_않으면_400_응답을_반환한다() throws Exception {
+            mockingAuthMemberInResolver();
+
+            mockMvc.perform(get("/v1/stores/followings")
+                            .header(AUTHORIZATION, BEARER + accessToken)
+                            .param("x", "123.3636")
+                            .param("deltaX", "3.12")
+                            .param("deltaY", "3.12"))
+                    .andDo(print())
+                    .andExpect(status().isBadRequest());
+        }
+
+        @Test
+        void 경도_증가값이_존재하지_않으면_400_응답을_반환한다() throws Exception {
+            mockingAuthMemberInResolver();
+
+            mockMvc.perform(get("/v1/stores/followings")
+                            .header(AUTHORIZATION, BEARER + accessToken)
+                            .param("x", "123.3636")
+                            .param("y", "32.3636")
+                            .param("deltaY", "3.12"))
+                    .andDo(print())
+                    .andExpect(status().isBadRequest());
+        }
+
+        @Test
+        void 위도_증가값이_존재하지_않으면_400_응답을_반환한다() throws Exception {
+            mockingAuthMemberInResolver();
+
+            mockMvc.perform(get("/v1/stores/followings")
+                            .header(AUTHORIZATION, BEARER + accessToken)
+                            .param("x", "123.3636")
+                            .param("y", "32.3636")
+                            .param("deltaX", "3.12"))
+                    .andDo(print())
+                    .andExpect(status().isBadRequest());
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"-1", "0"})
+        void 경도_증가값이_0이상의_양수가_아니라면_400_응답을_반환한다(String deltaX) throws Exception {
+            mockingAuthMemberInResolver();
+
+            mockMvc.perform(get("/v1/stores/followings")
+                            .header(AUTHORIZATION, BEARER + accessToken)
+                            .param("x", "123.3636")
+                            .param("y", "32.3636")
+                            .param("deltaX", deltaX)
+                            .param("deltaY", "3.12"))
+                    .andDo(print())
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("errorCode").value("CLIENT-101"))
+                    .andExpect(jsonPath("$.message").value(containsString("경도 증가값은 0이상의 양수만 가능합니다.")));
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"-1", "0"})
+        void 위도_증가값이_0이상의_양수가_아니라면_400_응답을_반환한다(String deltaY) throws Exception {
+            mockingAuthMemberInResolver();
+
+            mockMvc.perform(get("/v1/stores/followings")
+                            .header(AUTHORIZATION, BEARER + accessToken)
+                            .param("x", "123.3636")
+                            .param("y", "32.3636")
+                            .param("deltaX", "3.12")
+                            .param("deltaY", deltaY))
+                    .andDo(print())
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("errorCode").value("CLIENT-101"))
+                    .andExpect(jsonPath("$.message").value(containsString("위도 증가값은 0이상의 양수만 가능합니다.")));
+        }
     }
 }
