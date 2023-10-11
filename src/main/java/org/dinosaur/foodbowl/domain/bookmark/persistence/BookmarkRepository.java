7997bd3c10023e9aa7023d4bc706e85741324d73
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-10-12 01:34:59 +0900

    fix: 잘못 삭제된 findByMemberAndStore 복구

--- src/main/java/org/dinosaur/foodbowl/domain/bookmark/persistence/BookmarkRepository.java
+++ src/main/java/org/dinosaur/foodbowl/domain/bookmark/persistence/BookmarkRepository.java
@@ -1,12 +1,16 @@
 package org.dinosaur.foodbowl.domain.bookmark.persistence;
 
 import java.util.List;
+import java.util.Optional;
 import org.dinosaur.foodbowl.domain.bookmark.domain.Bookmark;
 import org.dinosaur.foodbowl.domain.member.domain.Member;
+import org.dinosaur.foodbowl.domain.store.domain.Store;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.Repository;
 
 public interface BookmarkRepository extends Repository<Bookmark, Long> {
+
+    Optional<Bookmark> findByMemberAndStore(Member member, Store store);
 
     @EntityGraph(attributePaths = {"store"})
     List<Bookmark> findByMember(Member member);
