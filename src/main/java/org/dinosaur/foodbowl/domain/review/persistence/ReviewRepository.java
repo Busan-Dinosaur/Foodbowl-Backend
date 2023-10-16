7a5ec5dd0d243bb7ada982c93388ca23bad79ac1
woo-chang <clllickme@naver.com>
2023-10-17 03:19:30 +0900

    feat: (#182) 멤버의 리뷰 데이터 삭제 기능 구현

--- src/main/java/org/dinosaur/foodbowl/domain/review/persistence/ReviewRepository.java
+++ src/main/java/org/dinosaur/foodbowl/domain/review/persistence/ReviewRepository.java
@@ -1,14 +1,25 @@
 package org.dinosaur.foodbowl.domain.review.persistence;
 
+import java.util.List;
 import java.util.Optional;
+import org.dinosaur.foodbowl.domain.member.domain.Member;
 import org.dinosaur.foodbowl.domain.review.domain.Review;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends Repository<Review, Long> {
 
     Optional<Review> findById(Long id);
 
+    List<Review> findAllByMember(Member member);
+
     Review save(Review review);
 
     void delete(Review review);
+
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Query("delete from Review r where r.member = :member")
+    void deleteByMember(@Param("member") Member member);
 }
