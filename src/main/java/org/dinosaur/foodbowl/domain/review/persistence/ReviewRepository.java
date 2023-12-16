6753d0ac991cd128b94225f6dba8ff51325da1f9
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-12-16 15:29:49 +0900

    feat: (#200) 멤버와 가게를 함께 조회하는 리뷰 단건 조회 메서드 추가

--- src/main/java/org/dinosaur/foodbowl/domain/review/persistence/ReviewRepository.java
+++ src/main/java/org/dinosaur/foodbowl/domain/review/persistence/ReviewRepository.java
@@ -2,11 +2,15 @@ package org.dinosaur.foodbowl.domain.review.persistence;
 
 import java.util.Optional;
 import org.dinosaur.foodbowl.domain.review.domain.Review;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.Repository;
 
 public interface ReviewRepository extends Repository<Review, Long> {
 
     Optional<Review> findById(Long id);
+
+    @EntityGraph(attributePaths = {"member", "store"})
+    Optional<Review> findWithStoreAndMemberById(Long id);
 
     Review save(Review review);
 
